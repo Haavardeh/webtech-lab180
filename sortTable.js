8ebff74948d3d904b41ebaf4fa99b20295959621
tiffany <ttmnguy@student.matnat.uio.no>
2022-01-18 18:29:08 +0100

    image

--- sortTable.js
+++ sortTable.js
@@ -1,12 +1,40 @@
-//var table = getElementByClass(tableClass);
-
 var a = "test connection";
 var b = "more lines";
 
 console.log(a);
 console.log(b);
+
 /*
-document.write("testing2");
+$('table').sortTable({
+})*/
+// = document.getElementById("products");
+
+function myCallback(){
+  console.log("You clicked the header");
+}
+
+//Function to make a header in the table clickable
+function headerClick(){
+  let table = $("#products");
+  $(table).find("th").click(myCallback);
+}
+
+
+//Waiting to jQuery to get ready before executing the code.
+$(function(){
+  let $table = document.getElementById("products");
+  console.log($($table));
+
+  headerClick();
+
+});
+
+
+
+
+
+
+/*
 function showRow(table){
   for (i in table){
     console.log(i);

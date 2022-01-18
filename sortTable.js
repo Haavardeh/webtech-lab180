08e91dbcd5c4a47568a23c7d73fb47cddefa723e
Haavardeh <58851371+Haavardeh@users.noreply.github.com>
2022-01-18 14:19:35 +0100

    Changed table type

--- sortTable.js
+++ sortTable.js
@@ -1,1 +1,15 @@
+//var table = getElementByClass(tableClass);
 
+var a = "test connection";
+var b = "more lines";
+
+console.log(a);
+console.log(b);
+/*
+document.write("testing2");
+function showRow(table){
+  for (i in table){
+    console.log(i);
+  }
+}
+*/

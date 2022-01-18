5bf6ba79260c9a5f244a752547c86c02d1dfbae9
Haavardeh <58851371+Haavardeh@users.noreply.github.com>
2022-01-18 15:36:19 +0100

    sending overthe code

--- sortTable.js
+++ sortTable.js
@@ -1,12 +1,25 @@
-//var table = getElementByClass(tableClass);
-
 var a = "test connection";
 var b = "more lines";
 
 console.log(a);
 console.log(b);
+
 /*
-document.write("testing2");
+$('table').sortTable({
+
+})*/
+// = document.getElementById("products");
+
+
+$(function(){
+  let $table = document.getElementById("products");
+  console.log($table);
+});
+
+
+
+
+/*
 function showRow(table){
   for (i in table){
     console.log(i);

cf7d72c8f2f33695f5719f482e4c2f5584137fca
Haavardeh <58851371+Haavardeh@users.noreply.github.com>
2022-01-19 19:03:50 +0100

    Reset button working

--- reset.js
+++ reset.js
@@ -1,1 +1,18 @@
 
+$(document).ready(function () {
+  $(".resetBtn").click(function() {
+    $.ajax({
+      url: 'https://wt.ops.labs.vu.nl/api22/572a7d65/reset',
+      type: "GET",
+      dataType: "json",
+      complete: function (data_response) {
+        alert(data_response.responseText);
+      },
+    });
+  });
+});
+
+/*
+Code found in first comment in this discussion thread from url:
+https://stackoverflow.com/questions/12322009/jquery-ajax-get-json/34142058
+*/

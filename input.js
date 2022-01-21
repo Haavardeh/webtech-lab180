f84c2d6a76b0a973634a0a1706dd210b258c59b5
tiffany <ttmnguy@student.matnat.uio.no>
2022-01-21 14:16:21 +0100

    reset btn

--- input.js
+++ input.js
@@ -1,1 +1,34 @@
+$(document).ready(function () {
+  $("form").submit(function (event) {
+    var formData = {
+      brand: $("#brand").val(),
+      model: $("#model").val(),
+      os: $("#os").val(),
+      image: $("#image").val(),
+      screensize: $("#screensize").val(),
+    };
 
+    $.ajax({
+      type: "POST",
+      url: "https://wt.ops.labs.vu.nl/api22/572a7d65/",
+      data: formData,
+      dataType: "json",
+      encode: true,
+    }).done(function (data) {
+      $("#products tr:last").load("index.html");
+
+    });
+
+
+    /*$.ajax({
+      url: 'https://wt.ops.labs.vu.nl/api22/572a7d65/',
+      type: "GET",
+      dataType: "json",
+      success: function(data) {
+        console.log($("data:last-child"));
+      }
+    });*/
+
+    event.preventDefault();
+  });
+});

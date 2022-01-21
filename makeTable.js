10d964b2582819b76686e60e045552fc788193ac
Haavardeh <58851371+Haavardeh@users.noreply.github.com>
2022-01-21 12:23:17 +0100

    something

--- makeTable.js
+++ makeTable.js
@@ -3,23 +3,32 @@ $(document).ready(function () {
       url: 'https://wt.ops.labs.vu.nl/api22/572a7d65/',
       type: "GET",
       dataType: "json",
-      complete: function(data){
-        console.log('<tr><td>${data.id}</td> <td>${data.brand}</td> <td><${data.model}</td> <td><${data.os}</td> <td><img src= ${data.image} ></td></tr>');
-      /*  var obj = data.responseText;
-        $.each(obj, function(i, data){
-
-          $("#formContent").append("<tr " +data.id + "<td>" ++ "</td><td>" + data.brand + "</td><td>" + data.model + "</td><td>"+ data.os + "</td><td><img src=" + data.image +""> >"</td></tr>");
-          alert (obj);
+      sucsess: function(data){
+        $.each(function(i, data){
+          $("#formContent").append("<tr <td> " + data[i].id + "</td><td>" + data[i].brand + "</td><td>" + data[i].model + "</td><td>"+ data[i].os + "</td><td><img[i] src=" + data.image + "> >" + "</td></tr>");
+        });
       }
-      }),
-      <td><img src=" + data.image + "></td>"
-
-
-      ${data.image}
-      */
-
     });
-});
+  });
+
+
+    // var obj = data.responseText;
+
+
+
+
+
+
+  //    <td><img src=" + data.image + "></td>"
+
+
+//});
+
+
+
+
+/*<tr><td>${data.id}</td> <td>${data.brand}</td> <td><${data.model}</td> <td><${data.os}</td> <td><img src= ${data.image} ></td></tr>'
+      ${data.image}*/
 
 
 /*let rows = document.createElement('tr');

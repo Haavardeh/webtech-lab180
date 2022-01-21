2e18fd706ec604d0c2e39f0b21df20076da869ad
tiffany <ttmnguy@student.matnat.uio.no>
2022-01-21 12:53:56 +0100

    reset btn

--- makeTable.js
+++ makeTable.js
@@ -3,15 +3,16 @@ $(document).ready(function () {
       url: 'https://wt.ops.labs.vu.nl/api22/572a7d65/',
       type: "GET",
       dataType: "json",
-      /*success: function(data) {
-        console.log(data[0].id);
-      }*/
-      success:
-        $.each(function(i, data){
-          $("#formContent").append(`<tr><td>${data[i].id}</td> <td>${data[i].brand}</td> <td>${data[i].model}</td> <td>${data[i].os}</td> <td><img src=" ${data[i].image} "></td></tr>`);
-      })
+      success: function(data) {
+        $.each(data, function(i, value){
+          $("#formContent").append(`<tr><td>${data[i].brand}</td> <td>${data[i].model}</td> <td>${data[i].os}</td> <td><img src=" ${data[i].image} "></td></tr>`);
+        });
+      }
     });
 });
+
+/* $("#formContent").append('<tr><td>'+${data[i].id}+'</td><td>'+${data[i].brand}+'</td><td>'+${data[i].model}+'</td><td>'+${data[i].brand}+'</td> <td><img src="' ${data[i].image} '"></td></tr>');*/
+//<td>${data[i].id}</td>
 
 
 /*let rows = document.createElement('tr');

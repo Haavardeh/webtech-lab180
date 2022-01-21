a32a4f1b90d088941652e0310587d938ffc9b311
tiffany <ttmnguy@student.matnat.uio.no>
2022-01-21 10:33:08 +0100

    reset btn

--- makeTable.js
+++ makeTable.js
@@ -1,1 +1,16 @@
+$(document).ready(function () {
+  $(".resetBtn").click(function() {
+    $.ajax({
+      url: 'https://wt.ops.labs.vu.nl/api22/572a7d65/',
+      type: "GET",
+      dataType: "json",
+      complete: function (data_response) {
+        alert(data_response.responseText);
+      },
+    });
+  });
+});
 
+
+/*let rows = document.createElement('tr');
+document.getElementById('tbody').appendChild(rows);*/

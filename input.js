6f70574fc0c7e3f48f7560d00ddc13181c4e7f38
tiffany <ttmnguy@student.matnat.uio.no>
2022-01-23 17:45:22 +0100

    reset btn

--- input.js
+++ input.js
@@ -16,11 +16,23 @@ $(document).ready(function () {
       encode: true,
     }).done(function (data) {
       $("#products").append(formData);
-      $("#products").load("index.html");
     });
 
+    /*$.get( "ajax/index.html", function( data ) {
+      $( "body" ).html( data );
+      alert( "Load was performed." );
+    });*/
+    $.ajax({
+      url: 'https://wt.ops.labs.vu.nl/api22/572a7d65/',
+      type: "GET",
+      dataType: "json",
+      success: function(data) {
+        i = data.length-1
+        $(".inputClass").before(`<tr><td>${data[i].brand}</td> <td>${data[i].model}</td> <td>${data[i].os}</td> <td id="phonePics"><img src="${data[i].image}" width=150px;></td> <td>${data[i].screensize}</td> </tr>`);
+      }
+    });
 
-    /*$.ajax({
+  /*  $.ajax({
       url: 'https://wt.ops.labs.vu.nl/api22/572a7d65/',
       type: "GET",
       dataType: "json",
